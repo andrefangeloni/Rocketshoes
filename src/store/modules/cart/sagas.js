ebacb9d226cfb49b3ec04a77afe3f163f2246872
andrefangeloni <andrefangeloni@gmail.com>
2019-07-29 22:59:47 -0300

    Notificando o estoque com o Toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, select, put, all, takeLatest } from "redux-saga/effects";
+import { toast } from "react-toastify";
 import api from "../../../services/api";
 import { formatPrice } from "../../../util/format";
 import { addToCartSuccess, updateAmount } from "./actions";
@@ -16,7 +17,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn("ERRO");
+    toast.error(`Somente ${currentAmount} disponível em estoque`);
     return;
   }
 
